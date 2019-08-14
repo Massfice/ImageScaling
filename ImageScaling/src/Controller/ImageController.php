a6dd8345b1e732db9833c7637f758f0706dc2ede
Massfice <39681631+Massfice@users.noreply.github.com>
2019-08-14 15:30:46 +0200

    final

--- ImageScaling/src/Controller/ImageController.php
+++ ImageScaling/src/Controller/ImageController.php
@@ -5,21 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Gumlet\ImageResize;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class ImageController extends AbstractController
 {
 
+  private function log(string $msg) {
+    $path = $this->getParameter('kernel.project_dir').'/public/logs/log.log';
+    $log = new Logger('name');
+    $log->pushHandler(new StreamHandler($path, Logger::INFO));
+
+    $log->info($msg);
+  }
+
   /**
    * @Route("/", name="show")
    */
-  public function show(Request $requrest)
+  public function show()
   {
-
-      // $this->
-      // return $this->json([
-      //     'message' => 'Welcome to your new controller!',
-      //     'path' => 'src/Controller/ImageController.php',
-      // ]);
 
       return $this->render('index.html.twig');
   }
@@ -27,11 +32,35 @@ class ImageController extends AbstractController
     /**
      * @Route("/resize", methods={"POST"}, name="resize")
      */
-    public function resize(Request $requrest)
+    public function resize(Request $request)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ImageController.php',
-        ]);
+
+        $this->log('----------------------Init Resizing----------------------');
+        $height = intval($request->get('height')); //Wysokość
+        $this->log('Set height to '.$height.'.');
+        $width = intval($request->get('width')); //Szerokość
+        $this->log('Set width to '.$width.'.');
+        $file = $request->files->get('file');
+
+        $path = $this->getParameter('kernel.project_dir').'/public/pictures';
+        if($file == NULL) {
+          $file = $path.'/default.jpg';
+          $this->log('Selecting picture was null, default would be used.');
+        }
+        else {
+          $file->move($path,'upload.jpg');
+          $name = $file->getClientOriginalName();
+          $file = $path.'/upload.jpg';
+          $this->log('Selecting picture would be used. Upload done. Name of picture was '.$name.'.');
+        }
+
+        $img = new ImageResize($file);
+
+        $img->resize($width, $height, true);
+        $img->save($path.'/picture.jpg');
+        $this->log('Used picture resized.');
+        $this->log('Picture displayed.');
+
+        return $this->render('index.html.twig',array('picture' => true));
     }
 }
